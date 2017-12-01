48eae72187a9dca1c03fef5a24fb4f793d88aba4
ArchibaldLeMagnifique <denis.lachartre@free.fr>
2017-12-01 19:00:29 +0100

    Coucou joli menu! Comment ca va joli menu?

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngAnimate']);
 
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', '$window', function($scope, $window) {
+    $scope.$window = $window;
+
     $scope.headMenu = ['Fichier', 'Edition', 'Format', 'Affichage'];
 
-    $scope.items = [{'name': 'Fichier', 'visible': 'none','items': ['Nouveau', 'Ouvrir', 'Enregistrer', 'Enregistrer sous']},
-        {'name': 'Edition', 'visible': 'none', 'items': ['Nouveau2', 'Ouvrir', 'Enregistrer', 'Enregistrer sous']},
-        {'name': 'Format', 'visible': 'none', 'items': ['Nouveau3', 'Ouvrir', 'Enregistrer', 'Enregistrer sous']},
-        {'name': 'Affichage', 'visible': 'none', 'items': ['Nouveau4', 'Ouvrir', 'Enregistrer', 'Enregistrer sous']}];
+    $scope.items = [{'name': 'Fichier', 'visible': false, 'margin': '259px', 'items': ['Partager...','', 'Nouveau', 'Ouvrir...', 'Renommer','Créer une copie...', 'Déplacer vers...', 'Placer dans la corbeille','', 'Historique des versions','','Télécharger au format','Publier sur le Web','Envoyer un e-mail aux collaborateurs...','Envoyer par e-mail en pièce jointe...','','Détails du document...','Langue','Configuration de la page','Imprimer']},
+        {'name': 'Edition', 'visible': false, 'margin': '374px', 'items': ['Nouveau2', 'Ouvrir', 'Enregistrer', 'Enregistrer sous']},
+        {'name': 'Format', 'visible': false, 'margin': '492px', 'items': ['Nouveau3', 'Ouvrir', 'Enregistrer', 'Enregistrer sous']},
+        {'name': 'Affichage', 'visible': false, 'margin': '609px', 'items': ['Nouveau4', 'Ouvrir', 'Enregistrer', 'Enregistrer sous']}];
 
+    $scope.mode = "normal";
 
-    $scope.openMenu = function(item) {
-        for (var i = 0; i < $scope.items.length; i++) {
-            $scope.items[i].visible = 'none';
-        }
+    $scope.openMenu = function(item, mode) {
+        $scope.mode = mode;
+        $scope.closeAll();
         switch(item) {
             case 'Fichier':
-                $scope.items[0].visible = 'table';
+                $scope.items[0].visible = true;
                 break;
             case 'Edition':
-                $scope.items[1].visible = 'table';
+                $scope.items[1].visible = true;
                 break;
             case 'Format':
-                $scope.items[2].visible = 'table';
+                $scope.items[2].visible = true;
                 break;
             case 'Affichage':
-                $scope.items[3].visible = 'table';
+                $scope.items[3].visible = true;
                 break;
         }
-    }
+    };
 
+    $scope.closeAll = function() {
+        for (var i = 0; i < $scope.items.length; i++) {
+            $scope.items[i].visible = false;
+        }
+    };
+
+    $scope.selection = "";
+
+    $scope.fire = function(item) {
+        $scope.selection = item.toString();
+    }
 }]);
+
+app.directive('ngRightClick', function($parse) {
+    return function(scope, element, attrs) {
+        var fn = $parse(attrs.ngRightClick);
+        element.bind('contextmenu', function(event) {
+            scope.$apply(function() {
+                event.preventDefault();
+                fn(scope, {$event:event});
+            });
+        });
+    };
+});
