270f4ef7d29e749d8b650d309e19f959324409f4
ArchibaldLeMagnifique <denis.lachartre@free.fr>
2017-12-08 15:39:32 +0100

    Ca marche a 100%

--- app.js
+++ app.js
@@ -17,14 +17,18 @@ app.controller('MainController', ['$scope', '$window', function($scope, $window)
                 'Remplacer l\'image','Reinitialiser l\'image']}];
 
     $scope.mode = "";
-
-    $scope.setMode = function(item, mode) {
-        $scope.mode = mode;
-        $scope.openMenu(item);
-    };
+    $scope.activeItems = [];
 
     $scope.openMenu = function(item) {
-        if ($scope.mode === "") return;
+        $scope.activeItems.push(item);
+        $scope.changeMenu(item);
+    };
+
+    $scope.changeMenu = function(item) {
+        if ($scope.activeItems.length <= 0) return;
+
+        $scope.activeItems = [item];
+        $scope.activableItems = $scope.getActivables();
 
         for (var i = 0; i < $scope.items.length; i++) {
             if ($scope.items[i].name === item.name) {
@@ -37,8 +41,24 @@ app.controller('MainController', ['$scope', '$window', function($scope, $window)
         }
     };
 
+    $scope.getActivables = function() {
+        var res = ['!Fichier', '!Format'];
+
+        for (var i=0; i<$scope.activeItems.length; i++) {
+            for (var j=0; j<$scope.activeItems[i].items.length; j++) {
+                if ($scope.activeItems[i].items[j].includes('!'))
+                    res += $scope.activeItems[i].items[j];
+            }
+        }
+        return res;
+    };
+    $scope.activableItems = $scope.getActivables();
+
+
     $scope.closeAll = function() {
         $scope.mode = "";
+        $scope.activeItems = [];
+        $scope.clickInit.x = -1;
         for (var i = 0; i < $scope.items.length; i++) {
             $scope.items[i].visible = false;
             // document.getElementById("item-"+i).className = "headMenuItem";
@@ -47,20 +67,64 @@ app.controller('MainController', ['$scope', '$window', function($scope, $window)
 
     $scope.selection = "";
 
-    $scope.fire = function(item) {console.log(item);
+    $scope.fire = function(item) {
         $scope.selection = item.replace('!','');
         $scope.closeAll();
+    };
+
+    $scope.lClickBackground = function(event) {
+        $scope.closeAll();
+
+    };
+
+    $scope.rClickBackground = function(event) {
+        $scope.closeAll();
+    };
+
+    $scope.clickInit = {x:-1,y:0};
+
+    $scope.mousepressedBackground = function(event) {
+        $scope.clickInit.x = event.clientX;
+        $scope.clickInit.y = event.clientY;
+    };
+
+    $scope.distance = function(a,b) {
+        return Math.sqrt( Math.pow((a.x-b.x), 2) + Math.pow((a.y-b.y), 2) );
+    };
+
+    $scope.getNearest = function(x,y,array) {
+        if (array.size<=0) return;
+        var item = array[0];
+
+        for (var i=0; i<array.length; i++) {
+
+            var p1 = {x:Math.min(Math.max(array[i].offsetLeft, x), array[i].offsetLeft+array[i].width) , y:Math.min(Math.max(array[i].offsetTop, y), array[i].offsetTop+array[i].height)};
+            var p2 = {x:Math.min(Math.max(item.offsetLeft, x), item.offsetLeft+item.offsetWidth) , y:Math.min(Math.max(item.offsetTop, y), item.offsetTop+item.offsetHeight)};
+
+            if ($scope.distance({x:x, y:y}, p1) < $scope.distance({x:x, y:y}, p2)) {
+                item = array[i];
+            }
+        }
+
+        return item;
     };
 
     $scope.size = 150;
 
     $scope.mouseMoved = function(event) {
+        if ($scope.clickInit.x !== -1) {
+            if ($scope.distance({x:event.clientX,y:event.clientY}, $scope.clickInit) > 10) {
+                $scope.mode = 'bubble';
+                var item = $scope.getNearest(event.clientX, event.clientY, document.getElementsByClassName('active'));
+                $scope.size = $scope.distance({x:event.clientX,y:event.clientY}, {x:item.offsetLeft,y:item.offsetTop})
+            }
+        }
+
         document.getElementById('mouseCircle').style.height = $scope.size.toString()+"px";
         document.getElementById('mouseCircle').style.width = $scope.size.toString()+"px";
 
         document.getElementById('mouseCircle').style.marginTop = (event.clientY - $scope.size/2).toString()+"px";
         document.getElementById('mouseCircle').style.marginLeft = (event.clientX - $scope.size/2).toString()+"px";
-
     };
 }]);
 
